fdfc67851036944aef09f58a7939601e52cbd508
Daniel Rojas Sánchez <daniel.rojsanch@gmail.com>
2024-09-14 23:19:21 -0600

    :pencil:

--- Scripts/plot1.jl
+++ Scripts/plot1.jl
@@ -6,6 +6,10 @@
 ## git remote add origin https://github.com/daniel-rojsanch/PlotsJL.git
 ## git push -u origin main
 
+## Intro to Plots in julia / Powerful convenience for visualization
+## Data visualization has a complicate history 
+
+## the gols are: Powerful, Intuitive, Concice, Flexible, Consistent, Lightweight, Smart
 
 using Plots
 using DataFrames
@@ -13,6 +17,7 @@ using CSV
 using XLSX
 using Dates
 using Statistics
+using UnicodePlots
 
 ## Daniel Rojas Sánchez
 ## Algunos Scripts para hacer graficos en julia
@@ -23,9 +28,19 @@ using Statistics
 x = [1, 4, 6, 3]
 y = x.^2
 
+
 scatter(x, y)
+
+
+
 
 ## graficar funciones
 
 f(x) = x^2
-plot(f, -4:.1:4 )
+plot(f, -4:.1:4 , lw = 3)
+title!("f(x) = x^2")
+
+savefig("Img/primer_grafico.svg")
+
+x_normal = randn(10000);
+histogram(x_normal)
